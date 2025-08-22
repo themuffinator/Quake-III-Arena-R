57e88f30d88a0f828d54f91bfe0d534148e28f55
f1am3d <flamesoff@gmail.com>
2025-08-22 19:06:47 +0200

    - fixed stb image resize dependency;

--- code/renderer/tr_image.c
+++ code/renderer/tr_image.c
@@ -57,7 +57,7 @@ static void* q3_stbi_realloc(void* p, size_t old_size, size_t new_size) {
 #define STBI_NO_GIF
 #include "stb_image.h"
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include "stb_image_resize.h"
+#include "stb_image_resize2.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
